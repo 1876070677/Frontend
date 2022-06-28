0449a67983560681d2d5f26f391d074c2079796c
1876070677 <1876070677@shbox.shop>
2022-06-29 00:40:24 +0900

    ADD : Border, Icon, BackgroundColor in login Option

--- src/naver/body.js
+++ src/naver/body.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './body.module.css';
+import id_login from './idLogin(activate).png';
 
 class Content extends Component {
     render() {
@@ -8,21 +9,22 @@ class Content extends Component {
                 <div className={styles.login_wrap}>
                     <ul className={styles.menu_wrap} role="tablist">
                         <li className={styles.menu_item} role="presentation">
-                            <a href="#none">
+                            <a href="#none" className={styles.menu_id}>
                                 <span className={styles.menu_text}>
+                                    <img src={id_login} className={styles.id_login_img} alt="id_login"></img>
                                     <span className={styles.text}>ID 로그인</span>
                                 </span>
                             </a>
                         </li>
                         <li className={styles.menu_item} role="presentation">
-                            <a href="#none">
+                            <a href="#none" className={styles.menu_ones}>
                                 <span className={styles.menu_text}>
-                                    <span className={styles.text}>일회용 번호</span>
+                                    <span className={styles.text}>일회용번호</span>
                                 </span>
                             </a>
                         </li>
                         <li className={styles.menu_item} role="presentation">
-                            <a href="#none">
+                            <a href="#none" className={styles.menu_qr}>
                                 <span className={styles.menu_text}>
                                     <span className={styles.text}>QR코드</span>
                                 </span>
