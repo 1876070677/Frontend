17921aa943598ef14b2a5bcda7d5ea2538f10259
1876070677 <1876070677@shbox.shop>
2022-06-30 00:39:02 +0900

    ADD : Login Option Nav Bar

--- src/naver/body.js
+++ src/naver/body.js
@@ -7,30 +7,49 @@ class Content extends Component {
         return (
             <div className='content'>
                 <div className={styles.login_wrap}>
-                    <ul className={styles.menu_wrap} role="tablist">
-                        <li className={styles.menu_item} role="presentation">
+                    <ul className={styles.menu_wrap}>
+                        <li className={styles.menu_item}>
                             <a href="#none" className={styles.menu_id}>
-                                <span className={styles.menu_text}>
+                                <div className={styles.img_wrap}>
                                     <img src={id_login} className={styles.id_login_img} alt="id_login"></img>
+                                </div>
+                                <span className={styles.menu_text}>
                                     <span className={styles.text}>ID 로그인</span>
                                 </span>
                             </a>
                         </li>
                         <li className={styles.menu_item} role="presentation">
                             <a href="#none" className={styles.menu_ones}>
+                                <div className={styles.img_wrap}>
+                                    <img src={id_login} className={styles.id_login_img} alt="id_login"></img>
+                                </div>
                                 <span className={styles.menu_text}>
-                                    <span className={styles.text}>일회용번호</span>
+                                    <span className={styles.text}>일회용 번호</span>
                                 </span>
                             </a>
                         </li>
                         <li className={styles.menu_item} role="presentation">
                             <a href="#none" className={styles.menu_qr}>
+                                <div className={styles.img_wrap}>
+                                    <img src={id_login} className={styles.id_login_img} alt="id_login"></img>
+                                </div>
                                 <span className={styles.menu_text}>
-                                    <span className={styles.text}>QR코드</span>
+                                    <span className={styles.text}>QR 코드</span>
                                 </span>
                             </a>
                         </li>
                     </ul>
+                    <form>
+                        <ul className={styles.panel_wrap}>
+                            <li className={styles.panel_item}>
+                                <div className={styles.panel_inner}>
+                                    <div className={styles.id_pw_wrap}>
+                                        
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </form>
                 </div>
             </div>
         )
