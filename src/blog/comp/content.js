2ecdf5f1da5842886b42f5f07b92c3516219d614
1876070677 <1876070677@shbox.shop>
2022-07-14 01:41:13 +0900

    MODIFY : Section Distribution

--- src/blog/comp/content.js
+++ src/blog/comp/content.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import styles from './content.module.css';
+import Home from '../section/section1';
 
 function Content() {
     return (
         <div className={styles.content_area}>
-            <div className={styles.ani_sec}>
-                <section className={styles.main_page}>
-                    <div className={styles.section_content}>
-                        
-                    </div>
-                </section>
+            <div className={styles.animated_sections}>
+                <Home />
             </div>
         </div>
     )
---
+++ src/blog/section/section1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './section1.module.css'
+
+function Home() {
+    return (
+        <section className={`${styles.animated_section} styles.main_page`}>
+            <div className={styles.section_content}>
+                
+            </div>
+        </section>
+    )
+}
+
+export default Home;
